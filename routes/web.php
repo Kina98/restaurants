841f8b4d46fd1d25262cb1095da8532917718851
Boubacar <boubacarkina1@gmail.com>
2024-01-27 02:29:37 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('restaurants', RestaurantController::class);
+
+// Route::get('/restaurants', [RestaurantController::class, 'index'])
+// ->name('restaurants.index');
+
+// Route::get('/restaurants/create', [RestaurantController::class, 'create'])
+//     ->name('restaurants.create');
+ 
+// Route::post('/restaurants', [RestaurantController::class, 'store'])
+//     ->name('restaurants.store');
+ 
+// Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'show'])
+//     ->name('restaurants.show');
+ 
+// Route::get('/restaurants/{restaurant}/edit', [RestaurantController::class, 'edit'])
+//     ->name('restaurants.edit');
+ 
+// Route::put('/restaurants/{restaurant}', [RestaurantController::class, 'update'])
+//     ->name('restaurants.update');
+ 
+// Route::delete('/restaurants/{restaurant}', [RestaurantController::class, 'destroy'])
+//     ->name('restaurants.destroy');
